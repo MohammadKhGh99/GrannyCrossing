80dfdf7d033367a421568558956c70a2e4b485ab
yairk <yair.klausner@mail.huji.ac.il>
2022-12-13 17:36:14 +0200

    fixed

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -14,6 +15,7 @@ public class Car : MonoBehaviour
    
     private Transform t;
     private int id;
+    private bool isStartGame;
     
     private Vector3 direction;
     private Vector3 startPosition;
@@ -23,12 +25,13 @@ public class Car : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        isStartGame = true;
         t = GetComponent<Transform>();
         spriteRenderer = t.GetComponent<SpriteRenderer>();
         if (controlPosition)
             t.position = new Vector3(x, y, 0);
         if (!withoutCars)
-            speed = !controlSpeed ? id is 0 or 9 ? Random.Range(7, 12) : Random.Range(12, 18) : speed;
+            speed = !controlSpeed ? Random.Range(7, 15) : speed;
 
         startPosition = t.position;
         fieldLimit = t.parent.GetComponentInParent<GameController>().GetFieldLimit();
@@ -37,6 +40,11 @@ public class Car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isStartGame)
+        {
+            isStartGame = false;
+            speed = !controlSpeed ? id is 0 or 9 ? Random.Range(7, 12) : Random.Range(12, 18) : speed;
+        }
         t.position += direction * (speed * Time.deltaTime);
         if (t.position.y > fieldLimit || t.position.y < -fieldLimit)
         {
@@ -76,4 +84,6 @@ public class Car : MonoBehaviour
     {
         return id;
     }
+
+    
 }
